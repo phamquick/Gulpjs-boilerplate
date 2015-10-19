3158b0cfc7ad67a4d05b754ee0ff018298d62bb4
Pham Thai Duong <mrtom90@me.com>
2015-10-19 17:56:49 +0900

    init

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var imagemin = require('gulp-imagemin');
+var sourcemaps = require('gulp-sourcemaps');
+var del = require('delete');
+
+var paths = {
+  scripts: ['src/js/**/*.js'],
+  images: 'src/img/**/*'
+};
+
+gulp.task('clean', function() {
+
+});
+
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+  gulp.src('./src/sass/**/*.scss')
+  	.pipe(sourcemaps.init())
+    	.pipe(sass().on('error', sass.logError))
+    	.pipe(sass({outputStyle: 'compressed'}))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./build/css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/sass/**/*.scss', ['sass']);
+});
+
+
+
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['clean','sass']);
